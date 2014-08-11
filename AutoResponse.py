f1a8d9914bbdf3adc872c1c641f2971ea28ba33a
Jonathan Cipriano <jcipriano@twitter.com>
2014-08-11 15:55:43 -0700

    Fixing stream related bugs.

--- AutoResponse.py
+++ AutoResponse.py
@@ -6,7 +6,7 @@ from tweepy import OAuthHandler
 from tweepy import Stream
 from tweepy import API
 
-import nltk.chat
+from nltk.chat import eliza
 
 config = ConfigParser.ConfigParser()
 config.read('.twitter')
@@ -16,6 +16,7 @@ consumer_secret = config.get('apikey', 'secret')
 access_token = config.get('token', 'token')
 access_token_secret = config.get('token', 'secret')
 stream_rule = config.get('app', 'rule')
+account_screen_name = config.get('app', 'account_screen_name').lower() 
 
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -26,44 +27,47 @@ chatbot = eliza.Chat(eliza.pairs)
 class ReplyToTweet(StreamListener):
 
     def on_data(self, data):
-        
-        print data
 
         jsonData = json.loads(data.strip())
 
+        # preamble - https://dev.twitter.com/docs/streaming-apis/messages#Friends_lists_friends
         friends = jsonData.get('friends')
         if friends:
-            # preamble
-            # https://dev.twitter.com/docs/streaming-apis/messages#Friends_lists_friends
             print 'Preamble received.'
             return
 
-        if jsonData.get('in_reply_to_status_id'):
-            # ignore reply tweets
-            print 'Ignoring reply Tweet.'
+        # deletes - deleting a Tweet will broadcast through the stream
+        delete = jsonData.get('friends')
+        if delete:
+            print 'Ignoring delete.'
+            return
+
+        # ignore Tweets from the app owner account
+        screenName = jsonData.get('user').get('screen_name')
+        if screenName.lower() == account_screen_name:
+            print 'Ignoring my own Tweets.'
             return
 
         tweetId = jsonData.get('id_str')
-        screenName = jsonData.get('user').get('screen_name')
-        userId = jsonData.get('user').get('id')
         tweetText = jsonData.get('text')
 
-        replyText = chatbot.respond(tweetText);
-        
-        print('Tweet ID: %s' % tweetId)
-        print('Screen Name: %s' % screenName)
-        print('User ID: %s' % userId)
-        print('Tweet Text: %s' % tweetText)
-        print('Reply Text: %s' % replyText)
-        print('')
+        replyText = '@' + screenName + ' ' + chatbot.respond(tweetText)
+
+        if len(replyText) > 140:
+            replyText = replyText[0:137] + '...'
+
+        print('Tweet ID: ' + tweetId)
+        print('From: ' + screenName)
+        print('Tweet Text: ' + tweetText)
+        print('Reply Text: ' + replyText)
 
         # If rate limited, the status posts should be queued up and sent on an interval
-        #twitterApi.update_status(replyText, tweetId)
+        twitterApi.update_status(replyText, tweetId)
 
-        return True
 
     def on_error(self, status):
         print status
+
 
 if __name__ == '__main__':
     r = ReplyToTweet()
